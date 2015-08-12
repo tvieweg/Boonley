0b51a2d8958d907efce8d138158a5735c94c5688
Trevor Vieweg <trevor.vieweg@gmail.com>
2015-08-11 20:56:25 -0700

    Initial commit

---
+++ Boonley/Boonley/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Boonley
+//
+//  Created by Trevor Vieweg on 8/11/15.
+//  Copyright (c) 2015 Trevor Vieweg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
