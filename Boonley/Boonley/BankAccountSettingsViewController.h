a4b8e17f7eb9199a37e29cd4c329060543fb05be
Trevor Vieweg <trevor.vieweg@gmail.com>
2015-09-15 07:52:53 -0700

    A couple updates

---
+++ Boonley/Boonley/BankAccountSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BankAccountSettingsViewController.h
+//  Boonley
+//
+//  Created by Trevor Vieweg on 9/14/15.
+//  Copyright (c) 2015 Trevor Vieweg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BankAccountSettingsViewController : UIViewController
+
+@end
---
+++ Boonley/Boonley/BankAccountSettingsViewController.m
@@ -0,0 +1,37 @@
+//
+//  BankAccountSettingsViewController.m
+//  Boonley
+//
+//  Created by Trevor Vieweg on 9/14/15.
+//  Copyright (c) 2015 Trevor Vieweg. All rights reserved.
+//
+
+#import "BankAccountSettingsViewController.h"
+
+@interface BankAccountSettingsViewController ()
+
+@end
+
+@implementation BankAccountSettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
